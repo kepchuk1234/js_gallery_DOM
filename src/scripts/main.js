7d92a146dcd1b6f45490281dcbd18327f27e170b
Bilokon Yaroslav <uvill.github@gmail.com>
2025-01-27 11:03:51 +0200

    add events to buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const mainPicture = gallery.querySelector('#largeImg');
+const buttons = [...gallery.querySelectorAll('.list-item')];
+
+for (const button of buttons) {
+  button.addEventListener('click', onClick);
+}
+
+function onClick(eventObject) {
+  eventObject.preventDefault();
+
+  const buttonSrc = this.querySelector('.list-item__link').getAttribute('href');
+
+  mainPicture.setAttribute('src', buttonSrc);
+}
